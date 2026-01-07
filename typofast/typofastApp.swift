b769d16d5130682df8263e153d780c97c722898e
blefo <lefort.baptiste@yahoo.fr>
2026-01-07 19:14:07 +0100

    Initial Commit

---
+++ typofast/typofastApp.swift
@@ -0,0 +1,17 @@
+//
+//  typofastApp.swift
+//  typofast
+//
+//  Created by Baptiste Lefort on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct typofastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ typofastTests/typofastTests.swift
@@ -0,0 +1,17 @@
+//
+//  typofastTests.swift
+//  typofastTests
+//
+//  Created by Baptiste Lefort on 07/01/2026.
+//
+
+import Testing
+@testable import typofast
+
+struct typofastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
